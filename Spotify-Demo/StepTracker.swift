d00e8f68c001e245a9b6b60ea96a3e44aef9971a
Anish Roy <anishroytx01@gmail.com>
2019-11-18 18:24:15 -0600

    got gitignore

---
+++ Spotify-Demo/StepTracker.swift
@@ -0,0 +1,9 @@
+//
+//  StepTracker.swift
+//  Spotify-Demo
+//
+//  Created by Anish Roy on 11/14/19.
+//  Copyright © 2019 Riverswave Technologies, India. All rights reserved.
+//
+
+import Foundation
